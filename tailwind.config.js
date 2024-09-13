3f994fc09f4464908cd3194534cb973d5c058ad4
Gigi Kuffa <jkuffa@wm.edu>
2024-09-13 02:48:38 -0400

    update README.md

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
